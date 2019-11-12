856a01311793f75cda748ba2a69230d94a5dc67d
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-12 05:27:31 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,9 +2,10 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
 
-  def initialize(name, owner)
+  def initialize(name, owner, mood)
     @name = name
     @owner = owner
+    @mood
   end
 
   def mood(mood = "nervous")
