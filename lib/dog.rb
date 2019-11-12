dcd1b10ea5d2f81c8e88c216c962d24afa47be8d
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-12 05:33:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,12 @@
 class Dog
-  attr_accessor :name, :owner, :mood
+  attr_accessor :owner, :mood
+  attr_reader :name
 
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
   end
+
 
 end
